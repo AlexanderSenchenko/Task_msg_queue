9f832d8463cb3c7a787ffb09c307039f000bde8f
AlexanderSenchenko <zayzsacha1998@gmail.com>
2019-08-20 17:17:02 +0700

    Broadcast message

--- chat/client_manager.c
+++ chat/client_manager.c
@@ -15,72 +15,6 @@ extern int id;
 extern int count_clients;
 int count = 0;
 
-static void client_connect(struct message* msg, struct list** head)
-{
-	struct clientctl* ptr;
-	struct clientctl bufsnd;
-	struct list* node;
-	struct message msg_snd;
-
-	int cltype;
-	int ret;
-	int i;
-
-	ptr = (struct clientctl*) msg;
-
-	count_clients++;
-	count++;
-
-	cltype = count + 100;
-
-	add_node(head, ptr->name, cltype);
-
-	printf("Count clients: %d\n", count_clients);
-	printf("%s connent\n", ptr->name);
-
-	bufsnd.type = CTL_TO_CLIENT;
-	bufsnd.id = cltype;
-
-	ret = msgsnd(id, (void*) &bufsnd, sizeof(struct clientctl), 0);
-	if (ret == -1)
-		perror("msgsnd");
-
-	msg_snd.type = (long) cltype;
-	msg_snd.act = MSG_USER_LIST;
-
-	node = *head;
-	printf("Pack:\n");
-	while (node != NULL) {
-		for (i = 0; (node != NULL) && (i < 4); ++i) {
-			strcpy(&(msg_snd.message[16 * i]), node->name);
-			printf("%s\n", node->name);
-			node = node->next;
-		}
-
-		msg_snd.size = i;
-
-		ret = msgsnd(id, (void*) &msg_snd, sizeof(struct message), 0);
-		if (ret == -1)
-			perror("msgsnd");
-	}
-}
-
-static void client_disconnect(struct message* msg, struct list** head)
-{
-	struct clientctl* ptr;
-	struct list* node;
-
-	ptr = (struct clientctl*) msg;
-
-	count_clients--;
-
-	node = search_node(*head, ptr->id);
-	printf("Count clients: %d\n", count_clients);
-	printf("%s disconnect\n", node->name);
-
-	del_node(head, ptr->id);
-}
-
 void client_manager()
 {
 	int ret;
@@ -92,7 +26,6 @@ void client_manager()
 	while (1) {
 		ret = msgrcv(id, (void*) &msgbuf, sizeof(struct message),
 				CTL_TO_SERVER, MSG_NOERROR);
-		printf("Size message %d\n", ret);
 		if (ret == -1) {
 			if (errno == EIDRM) {
 				perror("msgrcv");
@@ -113,13 +46,119 @@ void client_manager()
 				break;
 		}
 
+		#if 0
 		node = head;
 		printf("List clients:\n");
 		while (node != NULL) {
 			printf("Id %d Name: %s\n", node->id, node->name);
 			node = node->next;
 		}
+		#endif
 	}
 
 	free_list(head);
+}
+
+static void client_connect(struct message* msg, struct list** head)
+{
+	struct clientctl* ptr;
+
+	int cltype;
+
+	ptr = (struct clientctl*) msg;
+
+	count_clients++;
+	count++;
+
+	cltype = count + 100;
+
+	add_node(head, ptr->name, cltype);
+
+	printf("Count clients: %d\n", count_clients);
+	printf("%s connent\n", ptr->name);
+
+	snd_conf_info(cltype);
+
+	snd_list_user(*head, cltype);
+
+	broadcast(*head, cltype, MSG_USER_LIST, ptr->name, 16);
+}
+
+static void broadcast(struct list* head, int cltype, int act, char* str, int size)
+{
+	struct list* node = head;
+	struct message msg;
+	int ret;
+
+	msg.act = act;
+	msg.size = 1;
+	strncpy(msg.message, str, size);
+
+	while (node != NULL) {
+		if (node->id == cltype) {
+			node = node->next;
+			continue;
+		}
+
+		msg.type = (long) node->id;
+
+		ret = msgsnd(id, (void*) &msg, sizeof(struct message), 0);
+		if (ret == -1)
+			perror("msgsnd");
+
+		node = node->next;
+	}
+}
+
+static void snd_list_user(struct list* head, int cltype)
+{
+	struct list* node = head;
+	struct message msg;
+	int i;
+	int ret;
+
+	msg.type = (long) cltype;
+	msg.act = MSG_USER_LIST;
+
+	while (node != NULL) {
+		for (i = 0; (node != NULL) && (i < 4); ++i) {
+			strcpy(&(msg.message[16 * i]), node->name);
+			node = node->next;
+		}
+
+		msg.size = i;
+
+		ret = msgsnd(id, (void*) &msg, sizeof(struct message), 0);
+		if (ret == -1)
+			perror("msgsnd");
+	}
+}
+
+static void snd_conf_info(int cltype)
+{
+	struct clientctl msg;
+	int ret;
+
+	msg.type = CTL_TO_CLIENT;
+	msg.id = cltype;
+
+	ret = msgsnd(id, (void*) &msg, sizeof(struct clientctl), 0);
+	if (ret == -1)
+		perror("msgsnd");
+}
+
+static void client_disconnect(struct message* msg, struct list** head)
+{
+	struct clientctl* ptr;
+	struct list* node;
+
+	ptr = (struct clientctl*) msg;
+
+	count_clients--;
+
+	node = search_node(*head, ptr->id);
+	printf("Count clients: %d\n", count_clients);
+	printf("%s disconnect\n", node->name);
+
+	del_node(head, ptr->id);
 }
--- chat/client_manager.h
+++ chat/client_manager.h
@@ -3,6 +3,9 @@
 
 void client_manager();
 static void client_connect(struct message*, struct list**);
+static void broadcast(struct list*, int, int, char*, int);
+static void snd_list_user(struct list*, int);
+static void snd_conf_info(int);
 static void client_disconnect(struct message*, struct list**);
 
 #endif
